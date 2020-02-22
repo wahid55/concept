b91286c46a5d81bdfb9b515121a3c299d2344c25
WAHID <wahid.warid55@gmail.com>
2020-02-22 17:17:40 +0600

    Role model, seeder setup

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolesTableSeeder extends Seeder
 {
@@ -11,6 +12,23 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('roles')->delete();
+        DB::table('roles')->insert([
+            [
+                'name'          => 'Administrator',
+                "created_at"    => now(),
+                "updated_at"    => now()
+            ],
+            [
+                'name'          => 'Author',
+                "created_at"    => now(),
+                "updated_at"    => now()
+            ],
+            [
+                'name'          => 'Subscriber',
+                "created_at"    => now(),
+                "updated_at"    => now()
+            ]
+        ]);
     }
 }
